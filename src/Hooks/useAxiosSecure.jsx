1b7b283a7a0a94d8a70ef380332289c59fb2241a
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-06-22 11:16:42 +0600

    update link

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://food-restorent-server.onrender.com',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://food-restorent-server.onrender.com',
+    // baseURL: 'http://localhost:5000',
 });
 
 const useAxiosSecure = () => {
